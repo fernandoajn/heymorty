4578c07d1c0a2c066b44d03c5c0b244c716deec7
Fernando Augusto <fernandoajneto@gmail.com>
2020-05-02 19:49:10 -0300

    infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,37 +1,54 @@
 import React, {Component} from 'react';
 import './App.css';
 
-import api from './api';
+import api from './services/api';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 class App extends Component {
   state = {
-    chars: []
+    chars: [],
+    page: 1
+  }
+  
+  loadCharacters = async () => {
+    const res = await api.get('/character', {
+      params: {
+        page: this.state.page,
+      }
+    });
+    
+    console.log(res.data);
+    
+    this.setState(prevState => ({
+      chars: prevState.chars.concat(Array.from(res.data.results)),
+      page: prevState.page + 1
+    }));
   }
 
   componentDidMount() {
     this.loadCharacters();
   }
-  
-  loadCharacters = async () => {
-    const res = await api.get('/character');
-
-    this.setState({
-      chars: res.data.results
-    });
-  }
 
   render() {
     return (
-      <div className="App">
-        {this.state.chars.map(char => (
-          <div className="card">
-            <img key={char.id} src={char.image} alt={char.name} className="card__image"/>
-            <h3>{char.name}</h3>
-            <p>{char.species}</p>
-            <p>{char.origin.name}</p>
-          </div>
-        ))}
-      </div>
+      // <div className="App">
+        <InfiniteScroll className="App"
+          dataLength={this.state.chars.length}
+          next={this.loadCharacters}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+          >
+          {this.state.chars.map(char => (
+            <div className="card" key={char.id}>
+              <img  src={char.image} alt={char.name} className="card__image"/>
+              <h3>{char.name}</h3>
+              <p>{char.species}</p>
+              <p>{char.origin.name}</p>
+            </div>
+          ))}
+          {/* <button type="button" onClick={this.loadCharacters}>More</button> */}
+        </InfiniteScroll>
+      // </div>
     );
   }
 }
